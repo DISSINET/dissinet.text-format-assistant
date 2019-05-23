8cb5ffb511ac8a2fec21315229b55ca40b91fe6f
adammmertel <mertel.adam@gmail.com>
2019-05-23 14:44:24 +0200

    :art:STYLE: Styling lists

--- app/config.ts
+++ app/config.ts
@@ -26,11 +26,12 @@ Dotted line
 
 <div className="line"></div>
 
-Paragraph with indent
-<p className=" indent">Ut sed venenatis lacus. Vivamus convallis, velit ut tincidunt malesuada, metus massa pretium erat, vitae pulvinar dui nunc ac nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed in pulvinar risus, vitae iaculis purus. Cras ac nulla vel enim finibus ultrices a quis tellus. Quisque dapibus turpis odio, vel ornare mauris rhoncus eu. Vestibulum pharetra ex in libero ultrices dapibus. Praesent posuere, ex et eleifend porta, mi ex interdum odio, in dictum nunc augue quis tellus. </p>
+<p>
+PARAGRAPH Praesent tempor posuere euismod. Aenean vehicula sem vitae malesuada aliquet. Aliquam eros lectus, pretium a metus sit amet, fringilla scelerisque risus. Integer suscipit arcu eu velit tempor pulvinar. Aliquam viverra libero sit amet dolor blandit convallis. Aliquam sit amet dui elit. Mauris sit amet luctus sem. </p>
 
-Paragraph without indent
-<p>Praesent tempor posuere euismod. Aenean vehicula sem vitae malesuada aliquet. Aliquam eros lectus, pretium a metus sit amet, fringilla scelerisque risus. Integer suscipit arcu eu velit tempor pulvinar. Aliquam viverra libero sit amet dolor blandit convallis. Aliquam sit amet dui elit. Mauris sit amet luctus sem. </p>
+<p className=" indent">
+PARAGRAPH WITH INDENT Ut sed venenatis lacus. Vivamus convallis, velit ut tincidunt malesuada, metus massa pretium erat, vitae pulvinar dui nunc ac nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed in pulvinar risus, vitae iaculis purus. Cras ac nulla vel enim finibus ultrices a quis tellus. Quisque dapibus turpis odio, vel ornare mauris rhoncus eu. Vestibulum pharetra ex in libero ultrices dapibus. Praesent posuere, ex et eleifend porta, mi ex interdum odio, in dictum nunc augue quis tellus. </p>
+
 
 List without styling
 <ul className="list">
@@ -80,8 +81,6 @@ Link
 
 
 </div>
-
-
 
 `
 };
